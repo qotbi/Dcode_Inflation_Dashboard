35545e6323816a4bd455e9700e02a677a8f90084
qotbi <m.qotbi@dcodeefc.com>
2026-08-12 10:12:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // -----------------------------
 //filenames from gethub (place your CSVs in /data/)
 const FILES = {
-  inflationDetails: 'https://raw.githubusercontent.com/oelkaraksy/Dcode_Inflation_Dashboard/refs/heads/main/June%20Inflation%20Data.csv',
+  inflationDetails: 'https://raw.githubusercontent.com/qotbi/Dcode_Inflation_Dashboard/refs/heads/main/July%20Inflation%20Data.csv',
   annualHistory: 'https://raw.githubusercontent.com/oelkaraksy/Dcode_Inflation_Dashboard/refs/heads/main/Annual%20Inflation%20Historical%20Data.csv',
   monthlyHistory: 'https://raw.githubusercontent.com/oelkaraksy/Dcode_Inflation_Dashboard/refs/heads/main/Monthly%20Inflation%20Historical%20Data.csv'
 };
